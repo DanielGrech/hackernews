97f6d3aa97d947679dddc88a762e8ceee1f9df57
Daniel Grech <daniel.grech@vivant.com.au>
2015-09-23 18:10:52 +1000

    Added helpers to generate protobuf files

---
+++ generate_proto_golang.sh
@@ -0,0 +1,2 @@
+# brew install protobuf
+protoc --go_out=backend hackernews.proto
---
+++ generate_proto_java.sh
@@ -0,0 +1,1 @@
+java -jar wire-compiler-1.8.0.jar --proto_path=. --java_out=android/network/src/main/java hackernews.proto
