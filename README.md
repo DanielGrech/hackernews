918479d09bda7eef6352010e1d32ec89320dfa2d
Daniel Grech <daniel.grech@vivant.com.au>
2015-09-29 17:11:40 +1000

    Build-related fixes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 HN
 =========
+
+[![Build Status](https://travis-ci.org/DanielGrech/hackernews.svg?branch=master)](https://travis-ci.org/DanielGrech/hackernews)
--- build_and_test.sh
+++ build_and_test.sh
@@ -2,6 +2,8 @@
 
 #  Android tests
 
+echo "Going to execute Android tests.."
+
 cd android
 
 ./gradlew clean assembleDebug \
@@ -10,6 +12,18 @@ cd android
 	:data:testDebugUnitTest :data:testDebugUnitTestCoverage \
 	:app:testDevDebugUnitTest :app:testDevDebugUnitTestCoverage
 
+androidTestsResult=$?
+
 cd ..
 
+if [ $androidTestsResult -ne 0 ]; then
+    exit $androidTestsResult
+fi
+
 # Backend tests
+
+echo "Going to execute Backend tests.."
+
+cd backend
+
+cd ..
